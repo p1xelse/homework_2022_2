6bc94ed525337c0ddf2bea695caa0e71ed7cc916
p1xel <dima.neumoin1234@gmail.com>
2022-09-15 21:48:46 +0300

    add package json

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const debug = require('debug')
 
 const SERVER_PORT = 3002;
 
+const log = debug('server')
+
 const server = http.createServer((req, res) => {
     const {url} = req; // url = req.url деструктуризация
-    console.log('request', url);
+    log('request', url);
 
     const fileName = url === '/' ? '/index.html' : url;
     fs.readFile(`${__dirname}/../public${fileName}`, (err, data) => {
        if (err) {
-           console.log("error", url, 'not found')
+           log("error", url, 'not found')
            res.write('404 not found');
            res.end();
            return;
